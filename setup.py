ed7dbf2a60769260f64ecb40f3a8586e1fd58892
Matt Ranger <matthieu.ranger@outlook.com>
2020-02-27 11:16:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name="csrgraph",
     version="0.0.1",
     license='MIT',
-    description='Fast python graphs',
+    description='Tiny Library for Large Graphs',
     author='Matt Ranger',
-    url='https://github.com/VHRanger/graph2vec/',
+    url='https://github.com/VHRanger/CSRGraph/',
     packages=find_packages(),
-    # download_url='https://github.com/VHRanger/graph2vec/archive/0.0.1.tar.gz',
+    # download_url='https://github.com/VHRanger/CSRGraph/archive/0.0.1.tar.gz',
     keywords=['graph', 'network'],
     package_data={
         # If any package contains *.txt or *.rst files, include them:
