6426d61b7da83218762b18276718d09b74479879
Matt Ranger <matthieu.ranger@outlook.com>
2020-03-05 14:22:00 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 [![Build Status](https://travis-ci.com/VHRanger/CSRGraph.svg?branch=master)](https://travis-ci.com/VHRanger/CSRGraph)
 
+
 TODO:
 
-1) Break Node2Vec into separate class
+6) documentation // additional tests
+    predict_new
 
-2) Make SKLearnEmbedder
+5) beat node2vec at its own game
+    try UMAP, etc.
+
+5) TODOs in CSR (mmap, etc.)
+
+6) fastest graph Embed algo
+     GLoVe?
+     UMAP?
+     Other??
 
 3) Make Graph Layout Optimizer
+    How??
+    Ultra fast 2d embedder..?
--- setup.py
+++ setup.py
@@ -2,23 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="csrgraph",
-    version="0.0.1",
+    version="0.1.0",
     license='MIT',
-    description='Tiny Library for Large Graphs',
+    description='Fast python graphs',
     author='Matt Ranger',
-    url='https://github.com/VHRanger/CSRGraph/',
+    url='https://github.com/VHRanger/CSRGraph',
     packages=find_packages(),
-    # download_url='https://github.com/VHRanger/CSRGraph/archive/0.0.1.tar.gz',
     keywords=['graph', 'network'],
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.md', '*.txt', '*.rst']
     },
     install_requires=[
-    'networkx',
-    'numba',
-    'numpy',
-    'pandas',
-    'scipy',
+        'networkx',
+        'numba',
+        'numpy',
+        'pandas',
+        'scipy',
     ],
 )
