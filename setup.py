607f5a3025734772452310a3d6167b44c310a47d
Matt Ranger <20938940+VHRanger@users.noreply.github.com>
2020-06-03 14:10:10 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="csrgraph",
-    version="0.1.5",
+    version="0.1.16",
     license='MIT',
     description='Fast python graphs',
     author='Matt Ranger',
