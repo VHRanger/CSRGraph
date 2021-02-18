b6b48a0502eacf15475b1288229f64349341d5b7
Matt Ranger <20938940+VHRanger@users.noreply.github.com>
2021-02-18 17:59:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="csrgraph",
-    version="0.1.27",
+    version="0.1.28",
     license='MIT',
     description='Fast python graphs',
     author='Matt Ranger',
